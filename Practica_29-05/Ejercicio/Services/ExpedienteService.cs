8e363c28b6e2e9405018df2d8e91c3d2ca95a8e5
EmmaIsaac <emmanuel.i157@yahoo.com.ar>
2026-05-29 14:43:16 -0300

    Prueba

---
+++ Practica_29-05/Ejercicio/Services/ExpedienteService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio.Services
+{
+    internal class ExpedienteService
+    {
+        private string[] nombres;
+        private string[] DNI;
+        private double[] montos;
+        private int contadorIntegrados;
+
+        public ExpedienteService() 
+        {
+            nombres = new string[100];
+            DNI = new string[100];
+            montos = new double[100];
+            contadorIntegrados = 0;
+        }
+
+        public void RegistrarExpediente(string nombre, string dni, double monto) 
+        {
+
+        }
+    }
+}
